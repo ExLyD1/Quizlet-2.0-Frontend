be9dd667ef469dcfb9e1457a0207204dc018275d
ExLyD1_qq <sashaahnij@gmail.com>
2026-05-06 09:56:58 +0300

    feat: add default layout with Aside sidebar and Header

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex h-screen overflow-hidden bg-bg-base text-brand-pale">
+        <LayoutAside />
+        <div class="flex flex-1 flex-col overflow-hidden">
+            <LayoutHeader />
+            <main class="flex-1 overflow-y-auto">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
